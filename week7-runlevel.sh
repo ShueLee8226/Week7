f070c29d338ed62c33196322b59e18fdc7303865
root <root@debian.Slee>
2018-02-26 23:56:15 -0600

    scripting update

---
+++ week7-runlevel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+clear
+echo "Welcome to week 7 script"
+echo "Let determines your current run level"
+# by typing in runlevel
+runlevel
+sleep 2
+echo "Another similar command that whill show your"
+echo "current run level with the date and time"
+echo "is using the command who"
+# run the command who -r
+who -r
